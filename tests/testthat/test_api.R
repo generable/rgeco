b8e54ef297c77823b7a54e27aaad40349dcf5cd5
Jacqueline Buros <jackinovik@gmail.com>
2021-01-13 10:54:53 -0500

    Add test case for bad password

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -1,5 +1,9 @@
 library(testthat)
 
+testthat::test_that('login prints useful error message for incorrect password', {
+  testthat::expect_error(login(user = 'notauser', password = 'wrongpassword'), regexp = 'password')
+})
+
 testthat::test_that('project_versions endpoint works', {
   test_login()
   pv <- geco_api(PROJECTVERSIONS, project = TEST_PROJECT)
