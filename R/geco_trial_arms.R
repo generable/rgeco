39e006081d285df86ef002be344a3dbb01db1c1b
Jacqueline Buros <jackinovik@gmail.com>
2021-02-20 11:39:03 -0500

    add dedup step back in (accidentally removed)

--- R/geco_trial_arms.R
+++ R/geco_trial_arms.R
@@ -4,10 +4,16 @@
   trial_arms <- geco_api(TRIALARMS, project_version_id = pv_id)
   ta <- as_dataframe.geco_api_data(trial_arms, flatten_names = c('params', 'regimen'))
   if (nrow(ta) > 0) {
-    regimens <- .fetch_regimens_data(project_version_id = pv_id) %>%
-      dplyr::rename_all(.add_prefix, 'trial_arm')
-    ta <- ta %>%
-      dplyr::left_join(regimens, by = c('trial_arm_regimen_id'))
+    if ('regimen' %in% names(ta)) {
+      ta <- ta %>%
+        dplyr::mutate(trial_arm_regimen_id = .data$regimen$id) %>%
+        dplyr::select(-.data$regimen) %>%
+        dplyr::distinct()
+      regimens <- .fetch_regimens_data(project_version_id = pv_id) %>%
+        dplyr::rename_all(.add_prefix, 'trial_arm')
+      ta <- ta %>%
+        dplyr::left_join(regimens, by = c('trial_arm_regimen_id'))
+    }
     if ('params' %in% names(ta) && ncol(ta$params) > 0) {
       ta <- dplyr::bind_cols(ta, ta$params %>% dplyr::rename_all(~ stringr::str_c('trial_arm_', .x))) %>%
         dplyr::select(-.data$params)
