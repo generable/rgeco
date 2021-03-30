896fc7cf13248afa21c53931bb37deb5877a2ea1
Jacqueline Buros <jackinovik@gmail.com>
2021-03-30 16:16:05 -0400

    add fetch_inference_quantiles endpoint

--- R/inference_draws.R
+++ R/inference_draws.R
@@ -9,6 +9,20 @@ fetch_inference_draws <- function(project = NULL, project_version_id = NULL, run
   } else {
     draws <- geco_api(IDRAWS, project_version_id = pv_id, run_id=run_id, parameter=parameter, type=type)
   }
-  d <- convert_draws_to_df(draws)
+  d <- convert_draws_to_df(draws, name = parameter)
+}
+
+#' @importFrom magrittr %>%
+#' @importFrom rlang !!!
+fetch_inference_quantiles <- function(project = NULL, project_version_id = NULL, run_id, parameter, predictive = F, type = c('posterior', 'prior')) {
+  type <- match.arg(type, several.ok = F)
+  pv_id <- .process_project_inputs(project = project, project_version_id = project_version_id)
+  summarized_parameter = stringr::str_c('summarized_', parameter)
+  if (isTRUE(predictive)) {
+    draws <- geco_api(IPDRAWS, project_version_id = pv_id, run_id=run_id, parameter=summarized_parameter, type=type)
+  } else {
+    draws <- geco_api(IDRAWS, project_version_id = pv_id, run_id=run_id, parameter=summarized_parameter, type=type)
+  }
+  d <- convert_xarray_to_df(draws, name = summarized_parameter)
 }
 
--- R/utils_draws.R
+++ R/utils_draws.R
@@ -2,18 +2,19 @@
   reticulate::configure_environment(pkgname)
 }
 
-convert_draws_to_df <- function(resp) {
+
+convert_xarray_to_df <- function(resp, name = NULL) {
   xr <- reticulate::import('xarray', convert = FALSE)
   if (length(resp$content) > 0) {
     py_dict <- reticulate::r_to_py(resp$content)
     py_dataset <- xr$Dataset$from_dict(py_dict)
     py_df <- py_dataset$to_dataframe()$reset_index()
-    df <- reticulate::py_to_r(py_df) %>%
-      dplyr::rename(.chain = .data$chain,
-                    .iteration = .data$draw) %>%
-      dplyr::mutate(.chain = .data$.chain + 1,
-                    .iteration = .data$.iteration + 1,
-                    .draw = dplyr::dense_rank(stringr::str_c(.data$.chain, sprintf(.data$.iteration, fmt = '%04.0f'), sep = ':')))
+    df <- reticulate::py_to_r(py_df)
+    if (!is.null(name)) {
+      quo_name = rlang::sym(name)
+      df <- df %>%
+        tidyr::gather(.variable, .value, !!quo_name)
+    }
   } else {
     futile.logger::flog.info('No draws returned.')
     df <- tibble::tibble()
@@ -21,3 +22,38 @@ convert_draws_to_df <- function(resp) {
   df
 }
 
+convert_draws_to_df <- function(resp, name = NULL) {
+  df <- convert_xarray_to_df(resp, name = name)
+  if (nrow(df) > 0) {
+    df <- df %>%
+      dplyr::rename(.chain = .data$chain,
+                    .iteration = .data$draw) %>%
+      dplyr::mutate(.chain = .data$.chain + 1,
+                    .iteration = .data$.iteration + 1,
+                    .draw = dplyr::dense_rank(stringr::str_c(.data$.chain, sprintf(.data$.iteration, fmt = '%04.0f'), sep = ':')))
+  }
+  df
+}
+
+
+
+.width_from_quantile <- function(quantile) {
+  dplyr::case_when(quantile < 0.5 ~ 1 - round(2*quantile, digits = 2),
+                   quantile > 0.5 ~ 1 - round(2*(1-quantile), digits = 2),
+                   quantile == 0.5 ~ NA_real_)
+}
+
+format_quantiles_as_widths <- function(df) {
+  if (nrow(df) == 0) {
+    return(df)
+  }
+  df %>%
+    dplyr::mutate(.width = .width_from_quantile(quantile),
+                  .label = dplyr::case_when(quantile < 0.5 ~ '.lower',
+                                            quantile > 0.5 ~ '.upper',
+                                            quantile == 0.5 ~ '.median')) %>%
+    dplyr::select(-quantile) %>%
+    tidyr::spread(.label, .value) %>%
+    tidyr::fill(.median, .direction = 'updown') %>%
+    dplyr::filter(!is.na(.width))
+}
