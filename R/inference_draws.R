3c636ed2af9ba1a83b74477f7f7656caf3264622
Daniel Lee <daniel@generable.com>
2021-05-24 10:55:21 -0400

    doc: fetch_inference_draws

--- R/inference_draws.R
+++ R/inference_draws.R
@@ -1,9 +1,50 @@
 
-#' Fetch draws of parameter or predictive quantities for a model run
+#' Fetch draws from the Generable API for a run
+#'
+#' Fetch draws of the specified parameter or predictive quantity from the Generable API
+#' for a model run
+#'
+#' This function retrieves the draws from the Generable API for a model run. The user specifies
+#' the parameter or predictive quantity to query. A data.frame in long format is returned with
+#' `.chain`, `.iteration`, `.variable`, `.value`, and `.draw`.
+#'
+#' Note: this function may take a long time to return depending on the size of the parameter.
+#' If a summary of the parameter is sufficient, use \code{\link{fetch_inference_quantiles}} to
+#' access the quantiles of the draw.
+#'
+#' The parameters or predictive quantities for a particular model run can be found by calling
+#' \code{\link{fetch_inference_runs}}. The `run_quantiles` column in the returned `data.frame` contains
+#' two named lists describing the `parameter_names`or the `predictive_names`. When a predictive quantity
+#' is requested, i.e. the name is in the `predictive_names` list, the user must set the `predictive`
+#' argument in this function to `TRUE`.
+#' 
+#' Posterior draws are returned by default. The prior draws can be accessed by setting
+#' the `type` argument to `prior`.
+#'
+#' Authentication (see \code{\link{login}}) is required prior to using this function
+#' and this pulls the draws from the Generable API.
+#'
+#' A project can be specified by using the project name or a specific project version.
+#' If a project is specified using the name, data is fetched for the latest version of the project.
+#' If a project is specified using the project version, the project name is ignored if it
+#' is also included as an argument.
+#'
+#' @param parameter Name of the parameter or predictive quantity; this function does not take a vector.
+#' @param run_id Run id; required.
+#' @param project Project name
+#' @param project_version_id Project version. If this is specified, the `project` argument is ignored.
+#' @param predictive Set to `TRUE` if the parameter is a predictive quantity. Default is `FALSE`.
+#' @param type Type of quantile to return, either posterior or prior. Default is `posterior`. To access
+#'             prior quantiles, set this to `prior`.
+#' @return `data.frame` of draws in long format with `.chain`, `.iteration`, `.variable`, `.value`, and
+#'         `.draw`.
+#'
+#' @seealso \code{\link{fetch_inference_quantiles}}
+#'
 #' @importFrom magrittr %>%
 #' @importFrom rlang !!!
 #' @export
-fetch_inference_draws <- function(parameter, project = NULL, project_version_id = NULL, run_id = NULL, predictive = F, type = c('posterior', 'prior')) {
+fetch_inference_draws <- function(parameter, run_id = NULL, project = NULL, project_version_id = NULL, predictive = F, type = c('posterior', 'prior')) {
   type <- match.arg(type, several.ok = F)
   pv_id <- .process_project_inputs(project = project, project_version_id = project_version_id)
   if (is.null(run_id)) {
@@ -43,9 +84,8 @@ fetch_inference_draws <- function(parameter, project = NULL, project_version_id 
 #' Posterior quantiles are returned by default. The prior quantiles can be accessed by setting
 #' the `type` argument to `prior`.
 #'
-#' This function retrieves biomarker data from the Generable API.
-#' It requires authentication (see \code{\link{login}}) prior to use
-#' and this pulls data from the Generable API.
+#' Authentication (see \code{\link{login}}) is required prior to using this function
+#' and this pulls the quantiles from the Generable API.
 #'
 #' A project can be specified by using the project name or a specific project version.
 #' If a project is specified using the name, data is fetched for the latest version of the project.
@@ -61,6 +101,8 @@ fetch_inference_draws <- function(parameter, project = NULL, project_version_id 
 #'             prior quantiles, set this to `prior`.
 #' @return `data.frame` of quantiles in long format with `quantile`, `.variable`, and `.value` columns
 #'         for the 0.05, 0.10, 0.25, 0.50, 0.75, 0.90, and 0.95 quantile probabilities.
+#'
+#' @seealso \code{\link{fetch_inference_draws}}
 #'
 #' @importFrom magrittr %>%
 #' @importFrom rlang !!!
@@ -80,6 +122,6 @@ fetch_inference_quantiles <- function(parameter, run_id = NULL, project = NULL, 
   } else {
     quantiles <- geco_api(ITILES, project_version_id = pv_id, run_id=run_id, parameter=parameter, type=type)
   }
-  q <- convert_xarray_to_df(quantiles, name = parameter)
+  q <- convert_xarray_to_df(quantiles, name = parameter) %>% arrange(quantile)
 }
 
